0e2f39a904d5800a79c623914131be4f6b227d57
Arthur Elliott <clownpriest@gmail.com>
2018-06-06 10:39:43 -0400

    fix test

--- test/test.zig
+++ test/test.zig
@@ -22,9 +22,11 @@ test "strings" {
     var s2 = try string.init("this is some more data, SoMe some hey hey yo. APPLE DOG jump");
     var results = try s2.find_all("some");
 
-    // upper and lowercase
+    // check if contains substring
     assert(s2.contains("some"));
     assert(!s2.contains("apple"));
+
+    // upper and lowercase
     s2.lower();
     assert(mem.eql(u8, s2.buffer, "this is some more data, some some hey hey yo. apple dog jump"));
     s2.upper();
